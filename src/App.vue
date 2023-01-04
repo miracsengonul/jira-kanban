d0eba10296ea806eb969fba2ecdd910ff0eed475
mirac <miracsengonul@gmail.com>
2023-01-04 12:47:34 +0300

    google font style added

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@ body {
 #app {
   width: 100%;
   height: 100%;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
